7aae913798f99b2dbe06226a6490f43d661f6959
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-08-31 15:14:43 -0300

    feat: modularize index.js into components for better organization and reusability

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const database = require("./config/sqlContext.js");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Conectando ao banco de dados
+database.authDB();
+database.syncDB();
+
+// Importando as rotas
+const ownerRoutes = require('./routes/OwnerRoute');
+const freelancerRoutes = require('./routes/FreelancerRoute');
+
+// Usando as rotas
+app.use('/', ownerRoutes);
+app.use('/', freelancerRoutes);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,34 +1,5 @@
-require('dotenv').config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const database = require("./src/config/sqlContext.js");
+const app = require('./src/app');
 const port = process.env.PORT || 3002;
-
-// 
-app.use(cors());
-app.use(express.json());
-
-// Conectando ao banco de dados
-database.authDB();
-database.syncDB();
-
-// Rotas exemplo
-app.get("/teste", (req, res) => {
-    // Lógica para buscar freelancers do banco de dados
-    res.json([
-        { id: 1, name: "João" },
-        { id: 2, name: "Maria" },
-    ]);
-});
-
-// Importando as rotas
-const ownerRoutes = require('./src/routes/OwnerRoute');
-const freelancerRoutes = require('./src/routes/FreelancerRoute');
-
-// Usando as rotas
-app.use('/', ownerRoutes);
-app.use('/', freelancerRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
