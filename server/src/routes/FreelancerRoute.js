9719ad25428babcdaa6780dfeac833c8f507f0df
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-08-31 18:50:43 -0300

    fix(routes): fix bug in route order to correctly handle ID and non-ID requests

--- server/src/routes/FreelancerRoute.js
+++ server/src/routes/FreelancerRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const FreelancerController = require("../controllers/FreelancerController");
 
+router.get("/freelancers/:id", FreelancerController.getFreelancerById);
+
 router.get("/freelancers", FreelancerController.getAllFreelancers);
 
 router.post("/freelancers", FreelancerController.createFreelancer);
@@ -9,7 +11,5 @@ router.post("/freelancers", FreelancerController.createFreelancer);
 router.put("/freelancers/:id", FreelancerController.updateFreelancerById);
 
 router.delete("/freelancers/:id", FreelancerController.deleteFreelancerById);
-
-router.get("/freelancers/:id", FreelancerController.getFreelancerById);
 
 module.exports = router;
