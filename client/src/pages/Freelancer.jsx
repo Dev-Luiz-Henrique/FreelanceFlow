4f20d124e32ef16c48cdc918648f77d30be35eae
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-08-31 14:37:29 -0300

    feat(pages): add a GET request to fetch product list and display it

--- client/src/pages/Freelancer.jsx
+++ client/src/pages/Freelancer.jsx
@@ -1,36 +1,50 @@
 import { useState, useEffect } from "react";
-import statesEnum from "../types/StatesEnum";
 import Header from "../components/Header";
 
 const SignUp = () => {
-    const [data, setData] = useState([]);
+    const [freelancers, setFreelancers] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:3001/teste");  
-                const data = await response.json();
-                setData(data);
+                const response = await fetch("http://localhost:3001/freelancers");
+                if (!response.ok)
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                const fetchedData = await response.json();
+                setFreelancers(fetchedData);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error("Error fetching freelancers:", error);
             }
         };
 
-        fetchData();          
+        fetchData();
     }, []);
 
     return (
         <>
             <Header />
-            <main className='freelancer'>
-                {data.length > 0 && (
+            {freelancers.length > 0 && (
+                <main className='freelancer'>
                     <ul>
-                        {data.map((item, index) => (
-                            <li>{item.id} - {item.name}</li>
+                        {freelancers.map((freelancer) => (
+                            <li key={freelancer.id}>
+                            <strong>Nome:</strong> {freelancer.name}
+                            <br />
+                            <strong>Usuário:</strong> {freelancer.username}
+                            <br />
+                            <strong>Email:</strong> {freelancer.email}
+                            <br />
+                            <strong>Telefone:</strong> {freelancer.phone}
+                            <br />
+                            <strong>Estado:</strong> {freelancer.state}
+                            <br />
+                            <strong>Data de Nascimento:</strong> {freelancer.birthDate}
+                            {/* Adicione mais propriedades aqui conforme necessário */}
+                          </li>
                         ))}
                     </ul>
-                )}
-            </main>
+                </main>
+            )}
         </>
     );
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import SignUpForm from "../components/SignUpForm";
 import "../styles/SignUp.scss";
-import backgroundImage from "../assets/images/signup-background.png";
 
 const SignUp = () => {
     return (
@@ -9,12 +8,43 @@ const SignUp = () => {
             <Header />
             <main className='signup'>
                 <SignUpForm />
-                <div className='signup__img'>
-                    {/*<img src={backgroundImage} alt="imagem :)" />*/}
-                </div>
+                <section className='signup__info'>
+                    <div className='signup__content'>
+                        <h1>FreelanceFlow</h1>
+                        <p>Onde Talentos e Oportunidades se Encontram!</p>
+                        <div className='signup__cards'>
+                            <div className='signup__cards__card'>
+                                <p className='signup__cards__card__title'>
+                                    Escolha um Especialista
+                                </p>
+                                <p className='signup__cards__card__text'>
+                                    Tome decisões baseadas em avaliações a
+                                    portifólio
+                                </p>
+                            </div>
+                            <div className='signup__cards__card'>
+                                <p className='signup__cards__card__title'>
+                                    Comunique-se
+                                </p>
+                                <p className='signup__cards__card__text'>
+                                    Utilize as informações de contato para ter
+                                    conversas incríveis
+                                </p>
+                            </div>
+                            <div className='signup__cards__card'>
+                                <p className='signup__cards__card__title'>
+                                    Procure por projetos
+                                </p>
+                                <p className='signup__cards__card__text'>
+                                    Escolha os projetos que mais combinam com
+                                    você
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
 };
-
 export default SignUp;
